7c76e4229453f1d68870d9e481ac12519b94e13b
Eric Mogendi <mogendieric19@gmail.com>
2024-05-15 12:10:10 +0300

    Exercise 2.8: The Phonebook Step 3

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -3,20 +3,33 @@ import { useState } from 'react';
 const App = () => {
 	const [persons, setPersons] = useState([{ name: 'Arto Hellas' }]);
 	const [newName, setNewName] = useState('');
+	const [newNumber, setNewNumber] = useState('');
 
-	const handleSubmit = (event) => {
+	const handleNameChange = (event) => {
 		setNewName(event.target.value);
+	};
+	const handleNumberChange = (event) => {
+		setNewNumber(event.target.value);
 	};
 
 	const addName = (event) => {
 		event.preventDefault();
-		const existingPerson = persons.find((person) => person.name === newName);
+		const personExists = persons.find((person) => person.name === newName);
+		const numberExists = persons.find((person) => person.number === newNumber);
 
-		existingPerson
-			? alert(`${newName} is already added to phonebook`)
-			: setPersons(persons.concat({ name: newName }));
-
-		setNewName('');
+		if (personExists) {
+			alert(`${newName}  is already added to phonebook`);
+		} else if (numberExists) {
+			alert(`${newNumber}  is already added to phonebook`);
+		} else {
+			const personObject = {
+				name: newName,
+				number: newNumber,
+			};
+			setPersons(persons.concat(personObject));
+			setNewName('');
+			setNewNumber('');
+		}
 	};
 
 	return (
@@ -24,7 +37,10 @@ const App = () => {
 			<h2>Phonebook</h2>
 			<form onSubmit={addName}>
 				<div>
-					name: <input value={newName} onChange={handleSubmit} />
+					name: <input value={newName} onChange={handleNameChange} />
+					<div>
+						number: <input value={newNumber} onChange={handleNumberChange} />
+					</div>
 				</div>
 				<div>
 					<button type="submit">add</button>
@@ -32,7 +48,9 @@ const App = () => {
 			</form>
 			<h2>Numbers</h2>
 			{persons.map((person, index) => (
-				<p key={index}>{person.name}</p>
+				<p key={index}>
+					{person.name} {person.number}
+				</p>
 			))}
 		</div>
 	);
