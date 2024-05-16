5b2a9e5a8f2dfbe1a7cd8060fa7438f7c340ba2c
Eric Mogendi <mogendieric19@gmail.com>
2024-05-16 05:41:57 +0300

    Exercise 2.11: The Phonebook Step 6

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,6 @@
-import { useState } from 'react';
+/* eslint-disable no-mixed-spaces-and-tabs */
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Filter = ({ value, onChange }) => (
 	<div>
@@ -37,15 +39,22 @@ const Persons = ({ persons }) => (
 );
 
 const App = () => {
-	const [persons, setPersons] = useState([
-		{ name: 'Arto Hellas', number: '040-123456', id: 1 },
-		{ name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-		{ name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-		{ name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 },
-	]);
+	const [persons, setPersons] = useState([]);
 	const [newName, setNewName] = useState('');
 	const [newNumber, setNewNumber] = useState('');
 	const [newFilter, setNewFilter] = useState('');
+
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				const response = await axios.get('http://localhost:3001/persons');
+				setPersons(response.data);
+			} catch (error) {
+				console.log(error.message);
+			}
+		};
+		fetchData();
+	}, []);
 
 	const handleNameChange = (event) => {
 		setNewName(event.target.value);
